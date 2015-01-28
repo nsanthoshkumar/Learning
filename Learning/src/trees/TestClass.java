012c4795771f5995693c5b59dd177252b6c62eee
santhosh <sanatom.sjsu@gmail.com>
2015-01-28 08:36:01 -0800

    No.of leaves in a tree.

--- Learning/src/trees/TestClass.java
+++ Learning/src/trees/TestClass.java
@@ -5,8 +5,15 @@ public class TestClass {
 	public static void main(String[] args) {
 		Node root=new Node();
 		root.data=70;
+		root.leftChild=new Node();
+		root.leftChild.data=60;
+		root.rightChild=new Node();
+		root.rightChild.data=70;
+		root.rightChild.rightChild=new Node();
+		root.rightChild.rightChild.data=45;
 		TreeProblems tp=new TreeProblems();
 		System.out.println("Height: "+tp.getHeight(root));
+		System.out.println("leaves: "+tp.findNumOfLeaves(root));
 
 	}
 
